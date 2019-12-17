543f339b685c4e043d376b3490ba8b34601d087e
damien <d.macors@gmail.com>
2019-12-17 13:54:06 +0100

    tuto fini

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -40,11 +40,7 @@ async function login(parent, args, context, info) {
     }
 }
 
-module.exports = {
-    signup,
-    login,
-    post,
-};
+
 
 function post(parent, args, context, info) {
     const userId = getUserId(context);
@@ -54,3 +50,31 @@ function post(parent, args, context, info) {
         postedBy: { connect: { id: userId } },
     })
 }
+
+async function vote(parent, args, context, info) {
+    // 1 valider le jwt
+    const userId = getUserId(context);
+
+    // 2
+    const linkExists = await context.prisma.$exists.vote({
+        user: { id: userId },
+        link: { id: args.linkId },
+    });
+    if (linkExists) {
+        throw new Error(`Already voted for link: ${args.linkId}`)
+    }
+
+    // 3
+    return context.prisma.createVote({
+        user: { connect: { id: userId } },
+        link: { connect: { id: args.linkId } },
+    })
+}
+
+module.exports = {
+    signup,
+    login,
+    post,
+    vote
+};
+
