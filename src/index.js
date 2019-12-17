783863c887d133a090a5f12b7f93334e9a4dc8cb
damien <d.macors@gmail.com>
2019-12-17 10:43:33 +0100

    add prisma jwt

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}];
-let idCount = links.length;
+const { prisma } = require('./generated/prisma-clientnpm');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => links.find(({id}) =>args.id === id),
-    },
-    Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-            links.push(link);
-            return link
-        }
-    },
+    Query,
+    Mutation,
+    User,
+    Link
 };
 
-
-
-// 3
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
